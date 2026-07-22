5db090c50fc038066a675d2bf13943d3ff4c93db
inderjit-zerror <inderjit.zerror@gmail.com>
2026-07-22 17:59:27 +0530

    DONE

--- src/components/about/AboutHero.jsx
+++ src/components/about/AboutHero.jsx
@@ -35,18 +35,20 @@ const AboutHero = () => {
       <GridLine />
 
       <div className="w-full h-full overflow-hidden relative flex items-end p-4 sm:p-6 md:p-10">
-        <img
+        {/* <img
           src="https://images.unsplash.com/photo-1714703394123-73990ecd0d7c?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="IMG"
           className="w-full h-full object-cover object-bottom absolute top-0 left-0 z-1"
-        />
+        /> */}
+
+        <video src={`/video/AboutVideo.mp4`} muted loop autoPlay className="w-full h-full object-cover object-center  absolute top-0 left-0 z-1"></video>
 
         <h1 className="heading1 text-[2rem]! sm:text-[2.75rem]! md:text-[4rem]! lg:text-[5rem]! text-[#FBFBF7] relative z-20 leading-[1.05]!">
          Engineering Safer, Smarter Vehicles, <br className="hidden sm:block" />
           automotive technology company.
         </h1>
 
-         <div className="absolute inset-0 bg-gradient-to-t from-red-600/60 via-red-600/0 to-transparent z-10" />
+         {/* <div className="absolute inset-0 bg-gradient-to-t from-red-600/60 via-red-600/0 to-transparent z-10" /> */}
       </div>
     </div>
   );
